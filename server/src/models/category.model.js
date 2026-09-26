e9703a41e6365836bc7e5422b76c13f029959cd7
kambiDev <kambiseselias89@gmail.com>
2026-09-26 10:36:08 +0100

    feat(category): implement database queries for category model

--- server/src/models/category.model.js
+++ server/src/models/category.model.js
@@ -1,9 +1,48 @@
+import pool from "../config/db.js";
+
 class CategoryModel {
-  static async getAll() {}
-  static async getById(id) {}
-  static async create({ name, description }) {}
-  static async update({ name, description }) {}
-  static async delete(id) {}
+  static async getAll() {
+    const [rows] = await pool.execute(
+      "SELECT name, description FROM categories;",
+    );
+    return rows;
+  }
+
+  static async getById(id) {
+    const [rows] = await pool.execute(
+      "SELECT name, description FROM categories WHERE id = ?;",
+      [id],
+    );
+    return rows[0] ?? null;
+  }
+
+  static async create({ name, description }) {
+    const [result] = await pool.execute(
+      "INSERT INTO categories (name, description) VALUES (?, ?);",
+      [name, description],
+    );
+    return {
+      id: result.insertId,
+      name,
+      description,
+    };
+  }
+
+  static async update({ name, description, id }) {
+    const [result] = await pool.execute(
+      "UPDATE categories SET name = ?, description = ? WHERE id = ?;",
+      [name, description, id],
+    );
+    return result.affectedRows > 0;
+  }
+
+  static async delete(id) {
+    const [result] = await pool.execute(
+      "DELETE FROM categories WHERE id = ?;",
+      [id],
+    );
+    return result.affectedRows > 0;
+  }
 }
 
 export default CategoryModel;
